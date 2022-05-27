ec2eabe10bf369c724b5f33cbd9bb472f611a47f
amirsahra <amirhosein.sahra@gmail.com>
2022-05-27 12:07:30 +0430

    feat(test): create ConfigTest.php
    start TDD, create ConfigTest.php for testing Config.php class and methods,
    
    Commit number #4

---
+++ tests/unit/ConfigTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\unit;
+
+use App\helpers\Config;
+use PHPUnit\Framework\TestCase;
+
+class ConfigTest extends TestCase
+{
+    /*public function testGetContentConfigFileMethodReturnArray()
+    {
+        $contentConfigFile = Config::getContentConfigFile('database');
+        $this->assertIsArray($contentConfigFile);
+    }*/
+
+    /*
+    public function testGetConfigMethodReturnArray()
+    {
+
+    }
+
+    public function testGetConfigMethodReturnValidData()
+    {
+
+    }
+
+    public function testThrowsExceptionGetConfigMethodIfFileNotFound()
+    {
+
+    }
+
+    public function testConfigClassImplementConfigurableInterface()
+    {
+
+    }
+
+    public function testReceivedGetConfigMethodHaveRequiredKey()
+    {
+
+    }
+
+   */
+}
