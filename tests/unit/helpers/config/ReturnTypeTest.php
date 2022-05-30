0347ff2c7b16328f22f5af6ee28994b5a9472a1f
amirsahra <amirhosein.sahra@gmail.com>
2022-05-30 11:13:28 +0430

    feat(test): cut testGetContentConfigFileMethodReturnArray() to new class
    Insert the written function to test the config in the new class
    
    Commit number #11

--- tests/unit/helpers/config/ReturnTypeTest.php
+++ tests/unit/helpers/config/ReturnTypeTest.php
@@ -2,7 +2,21 @@
 
 namespace Tests\unit\helpers\config;
 
+use App\helpers\Config;
+
 class ReturnTypeTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @throws \App\exceptions\ConfigFileNotFoundException
+     */
+    public function testGetContentConfigFileMethodReturnArray()
+    {
+        $contentConfigFile = Config::getContentConfigFile('database');
+        $this->assertIsArray($contentConfigFile);
+    }
 
+   /* public function testGetConfigMethodReturnArray()
+    {
+
+    }*/
 }
